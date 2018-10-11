5974f8bc558f09f69e7ffd12c01f315578208533
Shayna Conner <sconner15@wou.edu>
2018-10-10 19:06:19 -0700

    LinkedQueue class

---
+++ HW3/LinkedQueue.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace HW3
+{
+    public class LinkedQueue<T> : IQueue<T>
+    {
+        private Node<T> front;
+        private Node<T> rear;
+
+        public LinkedQueue()
+        {
+            front = null;
+            rear = null;
+        }
+
+        public T Push(T element)
+        {
+            if (element == null) throw new NullReferenceException();
+
+            if (IsEmpty())
+            {
+                Node<T> temp = new Node<T>(element, null);
+                rear = front = temp;
+            }
+            else
+            {
+                Node<T> temp = new Node<T>(element, null);
+                rear.Next = temp;
+                rear = temp;
+            }
+
+            return element;
+        }
+
+        public T Pop()
+        {
+            T temp;
+
+            if (IsEmpty())
+            {
+                temp = front.Data;
+                front = null;
+                rear = null;
+            }
+            else
+            {
+                temp = front.Data;
+                front = front.Next;
+            }
+
+            return temp;
+        }
+
+        public bool IsEmpty()
+        {
+            return front == null && rear == null;
+        }
+    }
+}
