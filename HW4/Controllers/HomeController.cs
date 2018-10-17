bc8e68e66bdf2a2fd74604c232e29675ceac8ccf
Shayna Conner <sconner15@wou.edu>
2018-10-17 13:23:10 -0700

    Converter logic

--- HW4/Controllers/HomeController.cs
+++ HW4/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Web.Mvc;
+using Microsoft.Ajax.Utilities;
 
 namespace HW4.Controllers
 {
@@ -12,6 +14,42 @@ namespace HW4.Controllers
         [HttpGet]
         public ActionResult Converter()
         {
+            string milesInput = Request["miles"];
+            string unitsInput = Request["units"];
+            decimal convertedMiles = 0;
+
+            if (!milesInput.IsNullOrWhiteSpace())
+            {
+                decimal miles = 0;
+                try
+                {
+                    miles = Decimal.Parse(milesInput);
+                }
+                catch (Exception e)
+                {
+                    ViewBag.Error = e.ToString();
+                }
+
+                switch (unitsInput)
+                {
+                    case "millimeters":
+                        convertedMiles = miles * 1.609m;
+                        break;
+                    case "centimeters":
+                        convertedMiles = miles * 160934.4m;
+                        break;
+                    case "meters":
+                        convertedMiles = miles * 1609.344m;
+                        break;
+                    case "kilometers":
+                        convertedMiles = miles * 1.609m;
+                        break;
+                }
+            }
+
+            ViewBag.Converted = convertedMiles;
+            ViewBag.Units = unitsInput;
+
             return View();
         }
     }
