c90684d838601e66f4df721aecd562a86b22093b
Shayna Conner <sconner15@wou.edu>
2018-10-18 23:33:12 -0700

    Add in color translator stuff

--- HW4/Controllers/ColorController.cs
+++ HW4/Controllers/ColorController.cs
@@ -1,12 +1,42 @@
-﻿using System.Web.Mvc;
+﻿using System.Drawing;
+using System.Web.Mvc;
 
 namespace HW4.Controllers
 {
     public class ColorController : Controller
     {
-        public ActionResult Create()
+        public ActionResult Index()
         {
+            return View("Create");
+        }
+
+        [HttpPost]
+        public ActionResult Create(string firstColor, string secondColor)
+        {
+            Color firstRgb = ColorTranslator.FromHtml(firstColor);
+            Color secondRgb = ColorTranslator.FromHtml(secondColor);
+
+            int reds = LimitValue(firstRgb.R, secondRgb.R);
+            int greens = LimitValue(firstRgb.G, secondRgb.G);
+            int blues = LimitValue(firstRgb.B, secondRgb.B);
+
+            Color newColor = Color.FromArgb(reds, greens, blues);
+
+            ViewBag.ColorOne = firstRgb;
+            ViewBag.ColorTwo = secondRgb;
+            ViewBag.NewColor = newColor;
+
             return View();
+        }
+
+        // Ensures value of red, green, or blue value does not exceed 255
+        private int LimitValue(int first, int second)
+        {
+            const int max = 255;
+            int value = first + second;
+            if (value < max) return first + second;
+
+            return max;
         }
     }
 }
