676bde1592832aa48bc32d4f46b88584a02a843f
Shayna Conner <sconner15@wou.edu>
2018-10-23 23:11:43 -0700

    Started razor/HTML for results view

--- HW5/HW5/Controllers/RequestController.cs
+++ HW5/HW5/Controllers/RequestController.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Data.Entity;
 using System.Linq;
 using System.Web.Mvc;
 using HW5.DAL;
--- HW5/HW5/Models/TenantRequest.cs
+++ HW5/HW5/Models/TenantRequest.cs
@@ -1,25 +1,39 @@
 ﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace HW5.Models
 {
     public class TenantRequest
     {
+        [DisplayName("Request ID")]
         public int RequestId { get; set; }
 
+        [DisplayName("Date Requested")]
         public DateTime Created { get; set; }
 
         public string FirstName { get; set; }
 
         public string LastName { get; set; }
 
+        [DisplayName("Phone Number")]
         public string PhoneNumber { get; set; }
 
+        [DisplayName("Apartment Name")]
         public string ApartmentName { get; set; }
 
+        [DisplayName("Unit Number")]
         public int UnitNumber { get; set; }
 
+        [DisplayName("Comments")]
         public string Comments { get; set; }
 
+        [DisplayName("Entrance Allowed")]
         public bool IsEntrancePermitted { get; set; }
+
+        [NotMapped]
+        [DisplayName("Name")]
+        public string FullName { get { return $"{FirstName} {LastName}"; } }
+
     }
 }
