f439b1b5faea349714ad908ab6c78e1df8076583
Shayna Conner <sconner15@wou.edu>
2018-10-28 21:32:52 -0700

    LocalDb set up and nuget pkgs added

--- HW6/HW6/Global.asax.cs
+++ HW6/HW6/Global.asax.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Microsoft.SqlServer.Types;
 
 namespace HW6
 {
@@ -12,6 +13,11 @@ namespace HW6
     {
         protected void Application_Start()
         {
+            // For Spatial types, i.e. DbGeography
+            SqlServerTypes.Utilities.LoadNativeAssemblies(Server.MapPath("~/bin"));
+            // This next line is a fix that came from: https://stackoverflow.com/questions/13174197/microsoft-sqlserver-types-version-10-or-higher-could-not-be-found-on-azure/40166192#40166192
+            SqlProviderServices.SqlServerTypesAssemblyName = typeof(SqlGeography).Assembly.FullName;
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
