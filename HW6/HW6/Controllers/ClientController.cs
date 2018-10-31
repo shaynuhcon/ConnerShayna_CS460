23e7bac52b0df841e4d53e667dcaa554a7df5811
Shayna Conner <sconner15@wou.edu>
2018-10-30 23:45:48 -0700

    Controller methods and search results view

---
+++ HW6/HW6/Controllers/ClientController.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using HW6.DAL;
+using HW6.Models;
+using HW6.Models.ViewModels;
+
+namespace HW6.Controllers
+{
+    public class ClientController : Controller
+    {
+        public ActionResult Search()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Search(PeopleViewModel model)
+        {
+            using (var context = new WideWorldImportersContext())
+            {
+                model.PeopleResults = context.People.Where(x => x.FullName.Contains(model.Name)).ToList();
+            }
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public ActionResult Get(int id)
+        {
+            Person person;
+
+            using (var context = new WideWorldImportersContext())
+            {
+                person = context.People.FirstOrDefault(x => x.PersonID == id);
+            }
+
+            return View(person);
+
+        }
+    }
+}
---
+++ HW6/HW6/Models/ViewModels/PeopleViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace HW6.Models.ViewModels
+{
+    public class PeopleViewModel
+    {
+        public string Name { get; set; }
+
+        public IEnumerable<Person> PeopleResults { get; set; }
+    }
+}
