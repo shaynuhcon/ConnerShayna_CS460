4e447d54b4f39af30d4a7581756ded728eadc395
Shayna Conner <sconner15@wou.edu>
2018-11-04 17:51:17 -0800

    Comments

--- HW6/HW6/Models/ViewModels/PeopleViewModel.cs
+++ HW6/HW6/Models/ViewModels/PeopleViewModel.cs
@@ -4,8 +4,10 @@ namespace HW6.Models.ViewModels
 {
     public class PeopleViewModel
     {
+        // User search input
         public string Name { get; set; }
 
+        // List to store search results
         public IEnumerable<PersonSearchViewModel> PeopleResults { get; set; }
     }
 }
--- HW6/HW6/Models/ViewModels/PersonSearchViewModel.cs
+++ HW6/HW6/Models/ViewModels/PersonSearchViewModel.cs
@@ -2,8 +2,10 @@
 {
     public class PersonSearchViewModel
     {
+        // ID of person in database, not displayed to user
         public int PersonID { get; set; }
 
+        // Full name to display to user in view
         public string FullName { get; set; }
     }
 }
