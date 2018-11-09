d0d4f83485fb6e11a6bcdbd9622cc1ae6293c5c7
Shayna Conner <sconner15@wou.edu>
2018-11-08 17:08:24 -0800

    Stuff

---
+++ HW7/HW7/Controllers/TranslateController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+using RestSharp;
+
+namespace HW7.Controllers
+{
+    public class TranslateController : Controller
+    {
+
+        public ActionResult Sticker(string word)
+        {
+            RestClient client = new RestClient("https://api.giphy.com/v1/stickers/translate?api_key=KjJ8Sheq7lqXoB3zvB0pW0Qd8ZcFQYkq&s=lobster");
+            RestRequest request = new RestRequest(Method.GET);
+            IRestResponse response = client.Execute(request);
+            return View("Translator");
+        }
+    }
+}
