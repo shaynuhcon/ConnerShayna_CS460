0719b50fd6a54068d171f21bcef5b772d60b5efe
Shayna Conner <sconner15@wou.edu>
2018-11-08 21:24:57 -0800

    Scripts

--- HW7/HW7/Scripts/Translate.js
+++ HW7/HW7/Scripts/Translate.js
@@ -1,18 +1,16 @@
-﻿// Our Javascript goes here
-
-console.log("In numbers.js");
-
-var body = document.querySelector("body");
-body.addEventListener("keydown", function (e) {
-    var isInput = ~["TEXTAREA", "INPUT"].indexOf(e.target.tagName);
-    if (e.key === " " && !isInput) {
-        getData();
-        console.log("u clicked spacebar, and it wasn't in an input");
-    }
-});
+﻿var box = document.getElementById("words");
+box.addEventListener("keypress",
+    function(e) {
+        if (e.key === " " || e.key === "Spacebar") {
+            var words = $(this).text().split(" ");
+            var lastWord = words[words.length - 1];
+            console.log(lastWord);
+            getData();
+        }
+    });
 
 function getData() {
-    var word = $("#word").val();
+    var word = $("#words").val();
     console.log(word);
     var source = "/Translate/Translate/" + word;
     console.log(source);
@@ -27,10 +25,23 @@ function getData() {
 }
 
 function displayData(data) {
-    console.log(data);
-    var parsed = JSON.parse(data);
+    var obj = JSON.parse(data);
+    var url = obj.data.embed_url;
+    $("#output").text(url);
+    show_image(url);
 }
 
 function errorOnAjax() {
     console.log("error");
+}
+
+function show_image(src) {
+    var x = document.createElement("IMG");
+    var div = document.getElementById("output");
+
+    x.setAttribute("src", src);
+    x.setAttribute("width", "304");
+    x.setAttribute("height", "228");
+    x.setAttribute("alt", "Photo");
+    div.innerHTML += x;
 }
